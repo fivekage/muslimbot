dc9889dddfaa8f01d2f7515ec7532193805025a0
Samy Mokhtari <37446507+samymokhtari@users.noreply.github.com>
2023-12-27 18:40:06 +0100

    Added a radio quran in livestream which can be listen by everyone (#15)

--- utils/play_quran.js
+++ utils/play_quran.js
@@ -1,43 +1,112 @@
-const { Player, Track, QueryType } = require('discord-player');
 const { retrieveQuranVerses } = require('./retrieve_quran_verses.js')
+const prism = require('prism-media')
+const fs = require('fs')
+const {
+    NoSubscriberBehavior,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+    joinVoiceChannel,
+} = require('@discordjs/voice');
 const logger = require('./logger.js')
 
-module.exports.playQuran = async (client) => {
-    const sharedAudioPlayer = new Player(client);
 
-    logger.debug("Loading quran player")
-    await sharedAudioPlayer.extractors.loadDefault(e => e === "AttachmentExtractor");
+const player = createAudioPlayer({
+    behaviors: {
+        noSubscriber: NoSubscriberBehavior.Play,
+    },
+})
 
+
+
+
+module.exports.playQuran = (client = null) => {
     const quranVerses = retrieveQuranVerses();
     const randomVerse = quranVerses[Math.floor(Math.random() * quranVerses.length)]
     const fullPathFile = `./quran/${randomVerse}`
-
-    sharedAudioPlayer.events.on('playerStart', (queue, track) => {
-        logger.debug(`Started playing **${track.title}**!`);
+    const input = fs.createReadStream(fullPathFile);
+    const transcoder = new prism.FFmpeg({
+        args: [
+            '-analyzeduration', '0',
+            '-loglevel', '0',
+            '-f', 's16le',
+            '-ar', '48000',
+            '-ac', '2',
+        ],
     });
 
-    sharedAudioPlayer.events.on('disconnect', (queue) => {
-        logger.debug(`Disconnected!`);
-    });
+    const opus = new prism.opus.Encoder({ rate: 48000, channels: 1 });
 
+    input
+        .pipe(transcoder)
+        .pipe(opus);
 
-    try {
+    const name = randomVerse.replace('.mp3', '')
+    player.play(
+        createAudioResource(input, {
+            metadata: {
+                title: name
+            }
+        })
+    );
 
-        // const track = new Track(sharedAudioPlayer, {
-        //     searchEngine: QueryType.FILE,
-        //     queue: false,
-        //     nodeOptions: {
-        //         // nodeOptions are the options for guild node (aka your queue in simple word)
-        //         metadata: null // we can access this metadata object using queue.metadata later on
-        //     }
-        // })
-        logger.debug("Quran player loaded")
-
-    } catch (e) {
-        // let's return error if something failed
-        throw new Error(`Something went wrong: ${e}`)
+    if (client != null)
+        client.player = player
+    if (client && client.player != null) {
+        client.player.currentResource = name.slice(4, name.length)
     }
+
 }
 
+// Events
+player.on('stateChange', (oldState, newState) => {
+    logger.debug(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+});
+
+
+player.on(AudioPlayerStatus.Playing, (test) => {
+    logger.info(`Audio player is playing ${test.resource.metadata.title}`)
+});
+
+player.on(AudioPlayerStatus.Idle, (test) => {
+    logger.info(`Audio player is playing ${test.resource.metadata.title}`)
+    logger.info('Playback has stopped. Playing another surah.');
+    this.playQuran();
+});
+
+player.on('error', error => {
+    logger.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+    this.playQuran();
+});
+
+
+module.exports.connectToChannel = async (channel) => {
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+    try {
+        await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
+        // Close connection on disconnect
+        connection.on('stateChange', (_oldState, newState) => {
+            if (newState.status === VoiceConnectionStatus.Disconnected) {
+                try {
+                    connection.destroy();
+                    logger.info('Connection destroyed on:', connection.joinConfig.channelId);
+                } catch (error) {
+                    logger.error(error);
+                }
+            }
+        });
+        logger.info('New connection on:', connection.joinConfig.channelId);
+        return connection;
+    } catch (error) {
+        connection.destroy();
+        throw error;
+    }
+}
 
 
