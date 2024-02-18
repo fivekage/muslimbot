6ba9d18f8dd543247afd548aedef1ced9d809827
Samy Mokhtari <samy.mokhtari213@gmail.com>
2024-02-18 12:23:15 +0100

    minor/fixes

--- utils/retrieve_prayers.js
+++ utils/retrieve_prayers.js
@@ -3,12 +3,13 @@ module.exports.retrievePrayersOfTheDay = async (city, country, iso8601 = true) =
         const API_ENDPOINT_PRAYERS = `http://api.aladhan.com/v1/timingsByAddress?address=${city},${country}&iso8601=${iso8601 ? 'true' : 'false'}`
         fetch(API_ENDPOINT_PRAYERS)
             .then(async response => {
-                if (!response.ok || response.status !== 200) {
-                    return reject("Location not found")
+                if (!response.ok || (response.status < 200 && response.status >= 300)) {
+                    const json = await response.json()
+                    return reject(json.data ?? "Location not found")
                 }
 
-                const response_json = await response.json()
-                return response_json;
+                const json = await response.json()
+                return json;
             })
             .then(response => {
                 resolve(response['data']['timings'])
